155b2e761d3fe40c410e0d376ab0a5bdd3f1e902
Dor Katzir <katzirdor@gmail.com>
2024-02-02 10:57:25 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php $title="PHP-Course"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/php-elephant.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <h2>PHP - Complete Laracasts Course</h2>
+</body>
+</html>
