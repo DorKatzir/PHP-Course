2480c5004d121bd810724850b4e8ed169d5761c7
Dor Katzir <katzirdor@gmail.com>
2024-02-02 17:45:14 +0200

    Associative Arrays

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php 
-    $title="Arrays"; 
+    $title="Associative Arrays"; 
     $books = [
-        "Do Androids Dream of Electric Sheep",
-        "The Langoliers",
-        "Hail Mary"
+        [
+            "name" => "Do Androids Dream of Electric Sheep",
+            "year" => 1968,
+            "author" => 'Philip K. Dick',
+            "purchaseUrl" => '#',
+            "cover" =>"./assets/covers/androids.webp"
+        ],
+        [
+            "name" => "The Langoliers",
+            "year" => 1995,
+            "author" => 'Stephen King',
+            "purchaseUrl" => '#',
+            "cover" =>"./assets/covers/langoliers.jpg"
+        ],
+        [
+            "name" => "Project Hail Mary",
+            "year" => 2021,
+            "author" => 'Andy Weir',
+            "purchaseUrl" => '#',
+            "cover" =>"./assets/covers/hailMary.jpg"
+        ]
+    
     ];
 
 ?>
@@ -19,12 +38,19 @@
     <title><?= "PHP Course | $title" ?></title>
 </head>
 <body>
-    <h2>PHP - <?= $title ?></h2>
-    <h3>Recommended Books</h3>
-    <ul>
-        <?php foreach ($books as $book) : ?> 
-            <li><?= $book ?></li>
-        <?php endforeach; ?> 
-    </ul>
+    <h2 class="muted-text">PHP - <?= $title ?></h2>
+    <h3 class="muted-text">Recommended Books</h3>
+    <?php foreach ($books as $key => $book) : ?>
+        <div class="card">
+            <dl>
+                <dt class="muted-text">Book <?= $key+1 ?></dt>
+                <dd><span class="muted-text">Name:</span> <?= $book['name'] ?></dd>
+                <dd><span class="muted-text">Year:</span> <?= $book['year'] ?></dd>
+                <dd><span class="muted-text">Written by: </span> <?= $book['author'] ?></dd>
+            </dl>
+            <a class="button button1" href="<?= $book['purchaseUrl'] ?>">Buy this book</a>
+            <img class="cover" src="<?= $book['cover'] ?>" alt="">
+        </div> 
+    <?php endforeach; ?> 
 </body>
 </html>
