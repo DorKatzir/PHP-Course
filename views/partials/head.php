155eb276486f65c4b4a95fc98fde643ef1a3a8ba
Dor Katzir <katzirdor@gmail.com>
2024-02-03 17:23:49 +0200

    PHP Partials

---
+++ views/partials/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/php-elephant.svg" type="image/svg+xml">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+    <link rel="stylesheet" href="./assets/styles.css"> -->
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+
+<div class="min-h-full">
