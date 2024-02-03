d37da6ede017f7a521f7908982f41aa5c4a2256a
Dor Katzir <katzirdor@gmail.com>
2024-02-03 15:38:19 +0200

    Separate Logic From the Template

---
+++ index.view.php
@@ -0,0 +1,32 @@
+<?php 
+    $title="Lambda Functions"; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/php-elephant.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+    <link rel="stylesheet" href="./assets/styles.css">
+    <title><?= "PHP Course | $title" ?></title>
+</head>
+<body>
+    <h2 class="muted-text">PHP - <?= $title ?></h2>
+    <h3 class="muted-text">Books - Filtered by years between 1950-2020</h3>
+
+    <?php foreach ($filterByYear as $key => $book) : ?>
+        <div class="card">
+            <dl>
+                <dt class="muted-text"><?= $key+1 ?></dt>
+                <dd><span class="muted-text">Name:</span> <?= $book['name'] ?></dd>
+                <dd><span class="muted-text">Year:</span> <?= $book['year'] ?></dd>
+                <dd><span class="muted-text">Written by: </span> <?= $book['author'] ?></dd>
+            </dl>
+            <a class="button button1" href="<?= $book['purchaseUrl'] ?>">Buy this book</a>
+            <img class="cover" src="<?= $book['cover'] ?>" alt="">
+        </div> 
+    <?php endforeach; ?> 
+</body>
+</html>
