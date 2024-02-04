8c22e66653b4c281b988acfdfe2e0ed89d72f4f7
Dor Katzir <katzirdor@gmail.com>
2024-02-04 22:30:24 +0200

    Programming is Rewriting

--- controllers/notes.php
+++ controllers/notes.php
@@ -4,7 +4,7 @@ $heading = 'My Notes';
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$notes = $db->query("SELECT * FROM notes WHERE user_id = 1")->fetchAll();
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 1")->get();
 
 require "views/notes.view.php";
 
