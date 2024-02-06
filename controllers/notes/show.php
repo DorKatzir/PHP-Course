c36549f0e89656684d46cb4395cebed444fb9441
Dor Katzir <katzirdor@gmail.com>
2024-02-06 20:32:31 +0200

    Resourceful Naming Conventions

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -13,6 +13,6 @@ $note = $db->query("SELECT * FROM notes WHERE id = :id", ['id' => $_GET['id']])-
 authorize($note['user_id'] === $currentUserId);
 
 
-require "views/note.view.php";
+require "views/notes/show.view.php";
 
 
--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,7 +1,7 @@
 
-<?php require('partials/head.php'); ?>
-<?php require('partials/nav.php'); ?>
-<?php require('partials/banner.php'); ?>
+<?php require('views/partials/head.php'); ?>
+<?php require('views/partials/nav.php'); ?>
+<?php require('views/partials/banner.php'); ?>
 
 <main>
     <div class="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
@@ -38,5 +38,5 @@
     </div>
 </main>
 
-<?php require('partials/footer.php'); ?>
+<?php require('views/partials/footer.php'); ?>
 
