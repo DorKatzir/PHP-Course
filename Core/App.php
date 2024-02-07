130b0c4fc5b4154b6d07b848eca2f1f58d73124b
Dor Katzir <katzirdor@gmail.com>
2024-02-07 23:11:16 +0200

    Make Your First Service Containers

---
+++ Core/App.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace Core;
+
+class App {
+
+    static $container;
+
+    public static function setContainer($container)
+    {
+        static::$container = $container;
+    }
+
+
+    public static function container()
+    {
+        return static::$container;
+    }
+
+
+    public static function bind($key, $resolver)
+    {
+        static::container()->bind($ke, $resolver);
+    }
+
+    public static function resolve($key)
+    {
+        return static::container()->resolve($key);
+    }
+
+}
--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -3,9 +3,9 @@
 
 use Core\Database;
 use Core\Validator;
+use Core\App;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db= App::resolve(Database::class);
 
 $errors = [];
 
